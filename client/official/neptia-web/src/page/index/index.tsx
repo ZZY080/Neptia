b0abc64915e1acbe9ca70e44de9a6e96aef8fc19
zengzhiyuan <2916363651@qq.com>
2025-12-18 23:36:04 +0800

    feat: 修改首页

--- client/official/neptia-web/src/page/index/index.tsx
+++ client/official/neptia-web/src/page/index/index.tsx
@@ -11,21 +11,81 @@ export default function Index() {
 
   const indexData = t("index", { returnObjects: true }) as Record<string, any>;
   const services = [
-    "公司执照",
-    "签证",
-    "考察选址",
-    "税务行政",
-    "日常财税",
-    "税务筹划",
-    "人事",
-    "知识产权",
-    "法务",
-    "进出口许可证",
-    "产品认证",
-    "建筑工程",
-    "SIRIM",
-    "清真认证",
-    "ISO",
+    {
+      description: "公司执照",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/公司执照_1676127892861.png",
+    },
+    {
+      description: "签证",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/签证_1676127909436.png",
+    },
+    {
+      description: "考察选址",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "税务行政",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "日常财税",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "税务筹划",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "人事",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "知识产权",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "法务",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "进出口许可证",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "产品认证",
+      cover:
+        "	https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "建筑工程",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "SIRIM",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "清真认证",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
+    {
+      description: "ISO",
+      cover:
+        "https://server.shanhaimap.com/apis/jeecg-app/sys/common/static/product-category-files/2023-02-11/记账报税_1676127950167.png",
+    },
   ];
 
   return (
@@ -61,55 +121,9 @@ export default function Index() {
           </div>
         </div>
       </section>
-      <section className="px-4 py-12 bg-white">
-        <h2 className="text-2xl md:text-4xl text-center mb-8">我们的服务</h2>
 
-        <Swiper
-          centeredSlides
-          slidesPerView={1.2}
-          spaceBetween={20}
-          breakpoints={{
-            768: {
-              slidesPerView: 3,
-            },
-            1024: {
-              slidesPerView: 5,
-            },
-          }}
-          className="pb-8"
-        >
-          {[
-            "公司执照",
-            "签证",
-            "考察选址",
-            "税务行政",
-            "日常财税",
-            "税务筹划",
-          ].map((item) => (
-            <SwiperSlide key={item}>
-              <div
-                className="
-              transition-all duration-300
-              rounded-2xl border bg-gray-50
-              flex items-center justify-center
-              h-32 text-base
-              shadow-sm
-              swiper-slide-active:scale-120
-              swiper-slide-active:shadow-lg
-              swiper-slide-active:z-10
-              scale-95
-          
-
-            "
-              >
-                {item}
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </section>
-      <section className="w-full px-4 py-12 md:py-20 bg-white">
-        <div className="max-w-7xl mx-auto">
+      <section className="bg-white py-10">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {/* Title */}
           <h2 className="text-2xl md:text-4xl font-semibold text-gray-900 mb-8 md:mb-12 text-center">
             我们的服务
@@ -117,12 +131,17 @@ export default function Index() {
 
           {/* Service Grid */}
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-6">
-            {services.map((item) => (
+            {services.map((item, index) => (
               <div
-                key={item}
-                className="flex items-center justify-center text-center rounded-2xl border border-gray-200 bg-gray-50 px-3 py-6 text-sm md:text-base text-gray-800 shadow-sm hover:shadow-md transition"
+                key={index}
+                className="flex flex-col gap-4  items-center justify-center text-center rounded-2xl border border-gray-200 bg-gray-50 px-3 py-6 text-sm md:text-base text-gray-800 shadow-sm hover:shadow-md transition"
               >
-                {item}
+                <img
+                  className="w-[60px] h-[60px] sm:w-[60px] sm:h-[60px] mb-2 transition-transform group-hover:scale-80 object-cover"
+                  src={item.cover}
+                  alt=""
+                />
+                <div>{item.description}</div>
               </div>
             ))}
           </div>
@@ -131,7 +150,7 @@ export default function Index() {
 
       {/* Services Section */}
       <section className="bg-white py-8">
-        <div className="container mx-auto px-4">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-center mb-12">
             <motion.h2
               initial={{ opacity: 0, y: 30 }}
@@ -173,7 +192,7 @@ export default function Index() {
 
       {/* Testimonials Section */}
       <section className="bg-white py-8">
-        <div className="container mx-auto px-4">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-center mb-12">
             <motion.h2
               initial={{ opacity: 0, y: 30 }}
@@ -206,8 +225,8 @@ export default function Index() {
       </section>
 
       {/* CTA Section */}
-      <section className="bg-white py-8 text-center">
-        <div className="container mx-auto px-4">
+      <section className="bg-white py-8">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
